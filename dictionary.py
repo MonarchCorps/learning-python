aadfd1fdaa1d14e17816e50121d4a95bcc5dad05
MonarchCorps <monarchcorps01@gmail.com>
2025-09-14 12:52:14 +0100

    learnt about dictionaries and created a quiz game with tuples, loop and if statements

---
+++ dictionary.py
@@ -0,0 +1,34 @@
+capitals = {
+    "USA": "Washington D.C.",
+    "India": "New Delhi",
+    "Italy": "New York",
+    "China": "Beijing",
+    "Russia": "Moscow"
+}
+
+# if capitals.get("USA"):
+#     print(f"The capital of USA is {capitals.get('USA')}")
+# else:
+#     print("Capital of USA is undefined")
+
+# capitals.update({"India": "Indianapolis"})
+# capitals.pop("USA")
+# capitals.popitem()
+# capitals.clear()
+
+# keys = capitals.keys()
+# print(keys)
+
+# for key in capitals.keys():
+#     print(key)
+
+# print(capitals)
+
+# values = capitals.values()
+# for value in values:
+#     print(value)
+
+# items= capitals.items()
+# for key, value in capitals.items():
+#     print(f"{key}: {value}")
+
