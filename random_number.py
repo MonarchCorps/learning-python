abed3f1d841fcdacd92855c4769aa60f43d917bb
MonarchCorps <monarchcorps01@gmail.com>
2025-09-14 13:40:13 +0100

    Learnt about the random library

---
+++ random_number.py
@@ -0,0 +1,13 @@
+import random
+
+low = 1
+high = 100
+options = ("rock", "paper", "scissor")
+cards = ["2,", "3", "4", "T", "L"]
+
+# number =  random.randint(low, high)
+# number = random.random()
+# option = random.choice(options)
+
+random.shuffle(cards)
+print(cards)
